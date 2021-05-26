1e0555ca0684cc1c9054a6f18bf15c80b5e772de
masahito1997 <velonica.gale2227@gmail.com>
2021-05-26 17:16:55 +0900

    implement function to validate request

--- main.go
+++ main.go
@@ -1,24 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 )
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	dump, err := httputil.DumpRequest(req, true)
-	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Println(string(dump))
-	fmt.Fprintln(w, "test server")
+func main() {
+	http.HandleFunc("/calc/faverage", fluorescentAverageHandler)
+	http.HandleFunc("/calc/active_cells", activeCellHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func fluorescentAverageHandler(w http.ResponseWriter, req *http.Request) {
+	if validateRequest(w, req, http.MethodPost) != nil {
+		return
+	}
+}
+
+func activeCellHandler(w http.ResponseWriter, req *http.Request) {
+	if validateRequest(w, req, http.MethodPost) != nil {
+		return
+	}
+}
+
+func errorResponse(w http.ResponseWriter, err error, status int) {
+	http.Error(w, fmt.Sprint(err), status)
+}
+
+func validateRequest(w http.ResponseWriter, req *http.Request, expectedMethod string) error {
+	if req.Method != expectedMethod {
+		err := errors.New("access denied")
+		errorResponse(w, err, http.StatusBadRequest)
+		return err
+	}
+
+	dump, err := httputil.DumpRequest(req, true)
+	if err != nil {
+		errorResponse(w, err, http.StatusInternalServerError)
+		return err
+	}
+	fmt.Println(string(dump))
+	return nil
 }
